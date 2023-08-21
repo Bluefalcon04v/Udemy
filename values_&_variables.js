a25494494f7325c25c994b2e0dc5f5df4095d0eb
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-08-21 17:05:20 +0530

    first commit

---
+++ values_&_variables.js
@@ -0,0 +1,13 @@
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+
+
+let country = "India";
+let continent = "Asia";
+let population = 1430341556;
+
+console.log(country);
+console.log(continent);
+console.log(population);
