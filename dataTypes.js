b29e4f382cee52694e6aec68c47e4dc5e47cb953
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-08-22 18:19:09 +0530

    dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,17 @@
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+let isIsland = false;
+let country = "India";
+let continent = "Asia";
+let population = 1430341556;
+let language
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof continent); 
+console.log(typeof population);
+console.log(typeof language);
