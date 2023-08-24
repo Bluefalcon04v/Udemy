a61468cc38fdd6a559869af6406112bf47d24d04
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-08-24 14:12:21 +0530

    String and template literals

---
+++ strings&templateLiterals.js
@@ -0,0 +1,9 @@
+// 1. Recreate the 'description' variable from the last assignment, this time
+// using the template literal syntax
+
+let country = "India";
+const continent = "Asia";
+const language = "Hindi";
+let population = 1430341556;
+
+console.log(`${country} is in ${continent}, and it's ${population} people speak ${language}`);
